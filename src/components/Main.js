3d7ae653ca6d981cee75b17c09f13a0693a777df
qserena <tom@serenander.se>
2024-05-08 17:01:36 +0200

    Specified "outDir": "./dist

--- src/components/Main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-function Main({ darkMode }) {
-    return ((0, jsx_runtime_1.jsxs)("main", { className: darkMode ? 'dark' : '', children: [(0, jsx_runtime_1.jsx)("h1", { className: "main--title", children: "Fun facts about React" }), (0, jsx_runtime_1.jsxs)("ul", { className: "main--facts", children: [(0, jsx_runtime_1.jsx)("li", { children: "Was first released in 2013" }), (0, jsx_runtime_1.jsx)("li", { children: "Was originally created by Jordan Walke" }), (0, jsx_runtime_1.jsx)("li", { children: "Has well over 100K stars on GitHub" }), (0, jsx_runtime_1.jsx)("li", { children: "Is maintained by Facebook" }), (0, jsx_runtime_1.jsx)("li", { children: "Powers thousands of enterprise apps, including mobile apps" })] })] }));
-}
-exports.default = Main;
-//# sourceMappingURL=Main.js.map
