1255c029bc523c90a93c66c7430de14de8fd86a6
qserena <tom@serenander.se>
2024-05-08 16:48:40 +0200

    Set props types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { Navbar } from './components/Navbar'
 import Main from './components/Main'
 
 function App() {
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,6 @@
-import { NavbarProps } from './Navbar'
-
-export default function Main(props: NavbarProps) {
+export default function Main({ darkMode }: { darkMode: boolean }): JSX.Element {
     return (
-        <main className={props.darkMode ? 'dark' : ''}>
+        <main className={darkMode ? 'dark' : ''}>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
